c91a9eee37d33ace57addf78990e6c3bcf752cb5
devjpatel-growexxer <devj.patel@growexx.com>
2026-02-05 17:26:50 +0530

    Complete payment processor implementation

---
+++ payment.js
@@ -0,0 +1,12 @@
+class PaymentProcessor{
+    constructor(){
+        this.gateway = null;
+    }
+
+    processPayment(amount){
+        console.log(`Processing payment of ${amount}`);
+        return true;
+    }
+}
+
+module.exports = PaymentProcessor
